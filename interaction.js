0e4392c98792f53867b03c15a2e8def7074fc2ac
OscarGitH <pavoinepro@gmail.com>
2025-09-23 18:44:00 +0200

    feat: interaction onMouse

--- interaction.js
+++ interaction.js
@@ -3,10 +3,45 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+    this.canvas = canvas;
+    this.startX = 0;
+    this.startY = 0;
+    this.endX = 0;
+    this.endY = 0;
+    this.isDragging = false;
 
 	// Developper les 3 fonctions gérant les événements
+    this.onMouseDown = (evt) => {
+      const pos = getMousePosition(this.canvas, evt);
+      this.startX = pos.x;
+      this.startY = pos.y;
+      this.isDragging = true;
+      console.log(`Mouse down at ${this.startX}, ${this.startY}`);
+    };
+
+    this.onMouseMove = (evt) => {
+      if (this.isDragging) {
+        const pos = getMousePosition(this.canvas, evt);
+        this.endX = pos.x;
+        this.endY = pos.y;
+        console.log(`Mouse move at ${this.endX}, ${this.endY}`);
+      }
+    };
+
+  this.onMouseUp = (evt) => {
+    if (this.isDragging) {
+      const pos = getMousePosition(this.canvas, evt);
+      this.endX = pos.x;
+      this.endY = pos.y;
+      this.isDragging = false;
+      console.log(`Mouse up at ${this.endX}, ${this.endY}`);
+    }
+  };
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+    canvas.addEventListener('mousedown', this.onMouseDown, false);
+    canvas.addEventListener('mousemove', this.onMouseMove, false);
+    canvas.addEventListener('mouseup', this.onMouseUp, false);
 };
 
 
